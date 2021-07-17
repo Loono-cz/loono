2b359541353189678c85949d75b02dfc18079087
Duc Phi Viet <duc.phiviet01@gmail.com>
2021-07-17 16:51:04 +0200

    Wrap SafeArea to Scaffold body

--- lib/ui/screens/welcome.dart
+++ lib/ui/screens/welcome.dart
@@ -6,38 +6,39 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 100),
-            Image.asset(
-              'assets/icons/welcome-logo.png',
-              width: 172,
-              height: 213,
-            ),
-            const SizedBox(height: 50),
-            const Text(
-              'Chraň své zdraví včasnou prevencí',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF1A1919),
-                fontWeight: FontWeight.w400,
-                fontSize: 24,
-                letterSpacing: 0.5,
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 100),
+              Image.asset(
+                'assets/icons/welcome-logo.png',
+                width: 172,
+                height: 213,
               ),
-            ),
-            const SizedBox(height: 70),
-            OnboardingButton(label: 'Začít cestu za zdravím', onClick: () => print('Redirect to next page')),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () => print('Redirect to login'),
-              child: const Text('Už mám účet', style: TextStyle(color: Color(0xFF1A1919))),
-            ),
-          ],
+              const SizedBox(height: 50),
+              const Text(
+                'Chraň své zdraví včasnou prevencí',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF1A1919),
+                  fontWeight: FontWeight.w400,
+                  fontSize: 24,
+                  letterSpacing: 0.5,
+                ),
+              ),
+              const SizedBox(height: 70),
+              OnboardingButton(label: 'Začít cestu za zdravím', onClick: () => print('Redirect to next page')),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () => print('Redirect to login'),
+                child: const Text('Už mám účet', style: TextStyle(color: Color(0xFF1A1919))),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
