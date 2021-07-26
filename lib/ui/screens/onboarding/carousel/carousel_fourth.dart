47ab86140657effa86f9ad42f836558bd6be6d90
Matej Zidek <zidek.matt@gmail.com>
2021-07-26 20:44:07 +0200

    handle interactive widgets in the Stack

--- lib/ui/screens/onboarding/carousel/carousel_fourth.dart
+++ lib/ui/screens/onboarding/carousel/carousel_fourth.dart
@@ -6,32 +6,13 @@ class OnboardFourthCarouselScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(241, 249, 249, 1),
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(241, 249, 249, 1),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: CarouselContentWidget(
             text: 'Staneš se skutečným hrdinou / hrdinkou svého života.',
-            button: Padding(
-              padding: const EdgeInsets.only(bottom: 120.0),
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                width: double.infinity,
-                child: TextButton(
-                  style: ButtonStyle(
-                    alignment: Alignment.center,
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(const Color.fromRGBO(239, 173, 137, 1)),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/onboarding/gender');
-                  },
-                  child: const Text('Beru zdraví do svých rukou'),
-                ),
-              ),
-            ),
           ),
         ),
       ),
--- lib/ui/widgets/onboarding/carousel/story_page.dart
+++ lib/ui/widgets/onboarding/carousel/story_page.dart
@@ -9,20 +9,26 @@ class StoryPage extends StatelessWidget {
     Key? key,
     required this.content,
     this.duration = _defaultDuration,
+    this.interactiveContent,
     this.storyPageBackground = StoryPageBackground.light,
   }) : super(key: key);
 
   const StoryPage.dark({
     Key? key,
     required this.content,
+    this.interactiveContent,
     this.duration = _defaultDuration,
   })  : storyPageBackground = StoryPageBackground.dark,
         super(key: key);
 
   final Widget content;
+  final Widget? interactiveContent;
   final StoryPageBackground storyPageBackground;
   final Duration duration;
+
+  bool get hasInteractiveContent => interactiveContent != null;
 
   @override
   Widget build(BuildContext context) => content;
 }
+
