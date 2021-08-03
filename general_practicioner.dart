0657af359693a76fa589860197856f625b169b0d
Vojtěch Obořil <72251540+VojtechOboril@users.noreply.github.com>
2021-08-03 18:58:55 +0200

    Add files via upload

---
+++ general_practicioner.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:loono/ui/widgets/universal_doctor.dart';
+import 'package:loono/ui/widgets/progress_dots.dart';
+
+class OnboardingGeneralPracticionerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Expanded(
+                    child: LoonoProgressIndicator(
+                      numberOfSteps: 3,
+                      currentStep: 1,
+                    ),
+                  ),
+                  const SizedBox(width: 32),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/onboarding/doctor/gynecology');
+                    },
+                    child: const Text('přeskočit'),
+                  )
+                ],
+              ),
+              Expanded(
+                child: UniversalDoctor(
+                  questionHeader: 'Praktického lékaře?',
+                  imagePath: 'practicioner',
+                  nextCallback: () {
+                    Navigator.pushNamed(context, '/award');
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
