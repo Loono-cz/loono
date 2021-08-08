a9d56bf44f01db595fa6bbdb1c8797df0e80fdfe
smolikja <smolikja@protonmail.com>
2021-08-08 22:47:36 +0200

    progress dot update

--- lib/ui/widgets/progress_dots.dart
+++ lib/ui/widgets/progress_dots.dart
@@ -15,8 +15,8 @@ class LoonoProgressIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dots = <Widget>[];
-    for (var i = 0; i < numberOfSteps; i++) {
-      final text = (i + 1).toString();
+    for (var i = 1; i <= numberOfSteps; i++) {
+      final text = (i).toString();
       if (i < currentStep) {
         dots.add(const ProgressDot(style: ProgressDotStyle.done, text: "✔︎"));
       }
