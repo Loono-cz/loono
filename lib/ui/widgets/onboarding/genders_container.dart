8b75cba8dad9588d716db984182e1d50c6cdc113
Matej Zidek <zidek.matt@gmail.com>
2021-08-17 09:49:44 +0200

    make Bottom Sheet transparent

--- lib/ui/widgets/onboarding/genders_container.dart
+++ lib/ui/widgets/onboarding/genders_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:loono/constants.dart';
-import 'package:loono/ui/widgets/onboarding/gender_button.dart';
 import 'package:loono/l10n/ext.dart';
+import 'package:loono/ui/widgets/onboarding/gender_button.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 enum Gender { man, woman, other }
@@ -18,7 +18,10 @@ class GendersContainer extends StatefulWidget {
 }
 
 class _GendersContainerState extends State<GendersContainer> {
+  PersistentBottomSheetController? sheetController;
+
   Gender? activeButton;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +40,8 @@ class _GendersContainerState extends State<GendersContainer> {
                 height: 45,
                 onClick: () => setState(() {
                   activeButton = Gender.woman;
-                  widget.genderCallBack!(activeButton!);
+                  widget.genderCallBack!(activeButton);
+                  _closeSheet();
                 }),
               ),
             ),
@@ -52,7 +56,8 @@ class _GendersContainerState extends State<GendersContainer> {
                 height: 40,
                 onClick: () => setState(() {
                   activeButton = Gender.man;
-                  widget.genderCallBack!(activeButton!);
+                  widget.genderCallBack!(activeButton);
+                  _closeSheet();
                 }),
               ),
             ),
@@ -82,68 +87,76 @@ class _GendersContainerState extends State<GendersContainer> {
     const linearGradient = LinearGradient(colors: LoonoColors.rainbow);
     const textStyle = TextStyle(color: LoonoColors.black, height: 1.5);
 
-    showModalBottomSheet(
-      context: context,
-      backgroundColor: LoonoColors.bottomSheetLight,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      builder: (context) {
+    sheetController = Scaffold.of(context).showBottomSheet(
+      (context) {
         return FractionallySizedBox(
-          heightFactor: MediaQuery.of(context).size.height > 750 ? 0.7 : 0.881,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 24.0),
-                ShaderMask(
-                  shaderCallback: (bounds) => linearGradient.createShader(
-                    Rect.fromLTWH(0.0, 0.0, bounds.width, bounds.height),
+          heightFactor: MediaQuery.of(context).size.height > 750 ? 0.37 : 0.441,
+          child: Container(
+            decoration: BoxDecoration(
+              color: LoonoColors.bottomSheetLight,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 24.0),
+                  ShaderMask(
+                    shaderCallback: (bounds) => linearGradient.createShader(
+                      Rect.fromLTWH(0.0, 0.0, bounds.width, bounds.height),
+                    ),
+                    child: Text(
+                      context.l10n.gender_other.toUpperCase(),
+                      style: const TextStyle(color: Colors.white, fontSize: 24.0),
+                    ),
                   ),
-                  child: Text(
-                    context.l10n.gender_other.toUpperCase(),
-                    style: const TextStyle(color: Colors.white, fontSize: 24.0),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 35.0),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 18.0),
-                      Text(context.l10n.gender_other_sheet_desc, style: textStyle),
-                      const SizedBox(height: 22.0),
-                      RichText(
-                        text: TextSpan(
-                          text: '${context.l10n.gender_other_sheet_contact} ',
-                          style: textStyle,
-                          children: [
-                            TextSpan(
-                              text: LoonoStrings.contactEmail,
-                              style: textStyle.copyWith(decoration: TextDecoration.underline),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () async {
-                                  final Uri emailLaunchUri = Uri(
-                                    scheme: 'mailto',
-                                    path: LoonoStrings.contactEmail,
-                                  );
-                                  if (await canLaunch(emailLaunchUri.toString())) {
-                                    launch(emailLaunchUri.toString());
-                                  }
-                                },
-                            ),
-                            const TextSpan(text: '.', style: textStyle),
-                          ],
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35.0),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 18.0),
+                        Text(context.l10n.gender_other_sheet_desc, style: textStyle),
+                        const SizedBox(height: 22.0),
+                        RichText(
+                          text: TextSpan(
+                            text: '${context.l10n.gender_other_sheet_contact} ',
+                            style: textStyle,
+                            children: [
+                              TextSpan(
+                                text: LoonoStrings.contactEmail,
+                                style: textStyle.copyWith(decoration: TextDecoration.underline),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () async {
+                                    final Uri emailLaunchUri = Uri(
+                                      scheme: 'mailto',
+                                      path: LoonoStrings.contactEmail,
+                                    );
+                                    if (await canLaunch(emailLaunchUri.toString())) {
+                                      launch(emailLaunchUri.toString());
+                                    }
+                                  },
+                              ),
+                              const TextSpan(text: '.', style: textStyle),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         );
       },
     );
+  }
+
+  void _closeSheet() {
+    if (sheetController != null) {
+      sheetController!.close();
+      sheetController = null;
+    }
   }
 }
