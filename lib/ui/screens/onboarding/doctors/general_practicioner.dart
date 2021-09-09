b5a67378129e30b509bc5f4e2cedb9f4afb78465
Vojtěch Obořil <72251540+VojtechOboril@users.noreply.github.com>
2021-09-09 18:09:53 +0200

    194 (#23)
    * Loon 53/115
    
    * Add files via upload
    
    * Delete main.dart
    
    * Delete general_practicioner.dart
    
    * Delete award.dart
    
    * Added universal achievement screen and changed/added all of the achievement screens
    
    * removed old award screen
    
    * changed general practicioner screen from bad merge

--- lib/ui/screens/onboarding/doctors/general_practicioner.dart
+++ lib/ui/screens/onboarding/doctors/general_practicioner.dart
@@ -6,15 +6,15 @@ class OnboardingGeneralPracticionerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: const UniversalDoctorScreen(
-          questionHeader: "Praktického lékaře",
-          imagePath: "practicioner",
-          numberOfSteps: 3,
-          currentStep: 1,
-          nextScreen1: '/general_practicioner_achievement',
-          nextScreen2: '/onboarding/doctor/general-practitioner-date',
-          skipScreen: "/onboarding/doctor/gynecology",
-        )
+          child: const UniversalDoctorScreen(
+            questionHeader: "Praktického lékaře?",
+            imagePath: "practicioner",
+            numberOfSteps: 3,
+            currentStep: 1,
+            nextScreen1: "/general-practicioner-achievement",
+            nextScreen2: "/onboarding/doctor/general-practitioner-date",
+            skipScreen: "/onboarding/doctor/gynecology",
+          )
       ),
     );
   }
