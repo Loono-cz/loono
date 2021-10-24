4ade394d25c7d809ebcbbf24994745ba7b1e0053
smolikja <smolikja@protonmail.com>
2021-10-24 17:31:37 +0200

    avatar edit

--- lib/ui/widgets/settings/avatar.dart
+++ lib/ui/widgets/settings/avatar.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class LoonoAvatar extends StatelessWidget {
-  const LoonoAvatar({Key? key}) : super(key: key);
+  const LoonoAvatar({Key? key, this.radius = 50.0}) : super(key: key);
 
+  final double? radius;
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      radius: 50.0,
-      child: Text('AVATAR'),
+      radius: radius,
+      child: const Text('AVATAR'),
     );
   }
 }
