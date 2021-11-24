4c6b70f17727dba9312d727bf37da5046dc52ce6
Matej Zidek <zidek.matt@gmail.com>
2021-11-24 10:21:03 +0100

    add migrations

--- lib/services/db/database.dart
+++ lib/services/db/database.dart
@@ -14,12 +14,15 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase(String path, String password)
       : super(EncryptedExecutor.inDatabaseFolder(path: path, password: password));
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
   @override
   MigrationStrategy get migration => MigrationStrategy(onCreate: (Migrator m) {
         return m.createAll();
       }, onUpgrade: (Migrator m, int from, int to) async {
         debugPrint('**Moor** migration');
+        if (from == 1) {
+          m.addColumn(users, users.profileImageUrl);
+        }
       });
 
   Future<void> deleteAllData() {
