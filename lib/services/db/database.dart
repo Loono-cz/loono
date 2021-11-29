9d7d0e418549a4f1443248958c04d7f1a45bca6f
Matej Zidek <zidek.matt@gmail.com>
2021-11-29 19:40:38 +0100

    Revert "add migrations"
    This reverts commit 4c6b70f1

--- lib/services/db/database.dart
+++ lib/services/db/database.dart
@@ -14,15 +14,12 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase(String path, String password)
       : super(EncryptedExecutor.inDatabaseFolder(path: path, password: password));
   @override
-  int get schemaVersion => 2;
+  int get schemaVersion => 1;
   @override
   MigrationStrategy get migration => MigrationStrategy(onCreate: (Migrator m) {
         return m.createAll();
       }, onUpgrade: (Migrator m, int from, int to) async {
         debugPrint('**Moor** migration');
-        if (from == 1) {
-          m.addColumn(users, users.profileImageUrl);
-        }
       });
 
   Future<void> deleteAllData() {
