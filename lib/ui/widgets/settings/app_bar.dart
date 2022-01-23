292e5f4bbc5ba0f26d1bfc64e8856afe9084dfce
mattpake <pakematt@gmail.com>
2022-01-23 21:10:38 +0100

    app_bar_icons
    Changed app bar icons accordingly to design

--- lib/ui/widgets/settings/app_bar.dart
+++ lib/ui/widgets/settings/app_bar.dart
@@ -12,10 +12,18 @@ AppBar settingsAppBar(
     backgroundColor: Colors.transparent,
     elevation: 0,
     iconTheme: const IconThemeData(color: LoonoColors.black),
-    leading: showBackButton ? null : const SizedBox.shrink(),
+    leading: showBackButton
+        ? IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 32,
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          )
+        : const SizedBox.shrink(),
     actions: [
       IconButton(
-        icon: const Icon(Icons.close),
+        icon: const Icon(Icons.close, size: 37),
         onPressed: () => AutoRouter.of(context).navigate(closeRoute),
       ),
     ],
