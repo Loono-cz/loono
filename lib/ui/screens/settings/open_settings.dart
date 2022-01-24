170c0534052fab1697689596aaaa299c3be26406
MiroslavPeterka1 <miroslav.peterka1@gmail.com>
2022-01-24 05:08:07 +0100

    feat(LOON-444): add version to settings on the bottom_center

--- lib/ui/screens/settings/open_settings.dart
+++ lib/ui/screens/settings/open_settings.dart
@@ -10,6 +10,7 @@ import 'package:loono/ui/widgets/settings/app_bar.dart';
 import 'package:loono/ui/widgets/settings/avatar.dart';
 import 'package:loono/ui/widgets/settings/points_display.dart';
 import 'package:loono/utils/registry.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class OpenSettingsScreen extends StatelessWidget {
   OpenSettingsScreen({Key? key}) : super(key: key);
@@ -24,69 +25,85 @@ class OpenSettingsScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(left: 18.0, right: 18.0, bottom: 18.0),
-          child: SingleChildScrollView(
-            child: StreamBuilder<User?>(
-              stream: _usersDao.watchUser(),
-              builder: (context, snapshot) {
-                final userNickname = snapshot.data?.nickname;
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                child: StreamBuilder<User?>(
+                  stream: _usersDao.watchUser(),
+                  builder: (context, snapshot) {
+                    final userNickname = snapshot.data?.nickname;
 
-                return Column(
-                  children: [
-                    LoonoAvatar(radius: 60.0),
-                    const SizedBox(height: 24.0),
-                    if (userNickname != null)
-                      Column(
-                        children: [
-                          Text(
-                            userNickname,
-                            style: const TextStyle(fontSize: 24),
-                          ),
-                          const SizedBox(height: 24.0),
-                        ],
-                      ),
-                    LoonoButton.light(
-                      text: context.l10n.settings_edit_account_button,
-                      onTap: () {
-                        AutoRouter.of(context).push(UpdateProfileRoute());
-                      },
-                    ),
-                    const SizedBox(height: 104.0),
-                    PointsDisplay(),
-                    const SizedBox(height: 12.0),
-                    Text(
-                      context.l10n.points_your_points_desc.toUpperCase(),
-                      style: const TextStyle(
-                        fontSize: 11,
-                        color: LoonoColors.black,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    const SizedBox(height: 48.0),
-                    Row(
+                    return Column(
                       children: [
-                        Expanded(
-                          child: LoonoButton.light(
-                            text: context.l10n.settings_user_points_help_button,
-                            onTap: () {
-                              AutoRouter.of(context).push(const PointsHelpRoute());
-                            },
+                        LoonoAvatar(radius: 60.0),
+                        const SizedBox(height: 24.0),
+                        if (userNickname != null)
+                          Column(
+                            children: [
+                              Text(
+                                userNickname,
+                                style: const TextStyle(fontSize: 24),
+                              ),
+                              const SizedBox(height: 24.0),
+                            ],
+                          ),
+                        LoonoButton.light(
+                          text: context.l10n.settings_edit_account_button,
+                          onTap: () {
+                            AutoRouter.of(context).push(UpdateProfileRoute());
+                          },
+                        ),
+                        const SizedBox(height: 104.0),
+                        PointsDisplay(),
+                        const SizedBox(height: 12.0),
+                        Text(
+                          context.l10n.points_your_points_desc.toUpperCase(),
+                          style: const TextStyle(
+                            fontSize: 11,
+                            color: LoonoColors.black,
+                            fontWeight: FontWeight.w700,
                           ),
                         ),
-                        const SizedBox(width: 8.0),
-                        Expanded(
-                          child: LoonoButton.light(
-                            text: context.l10n.leaderboard,
-                            onTap: () {
-                              AutoRouter.of(context).push(const LeaderboardRoute());
-                            },
-                          ),
-                        )
+                        const SizedBox(height: 48.0),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: LoonoButton.light(
+                                text: context.l10n.settings_user_points_help_button,
+                                onTap: () {
+                                  AutoRouter.of(context).push(const PointsHelpRoute());
+                                },
+                              ),
+                            ),
+                            const SizedBox(width: 8.0),
+                            Expanded(
+                              child: LoonoButton.light(
+                                text: context.l10n.leaderboard,
+                                onTap: () {
+                                  AutoRouter.of(context).push(const LeaderboardRoute());
+                                },
+                              ),
+                            )
+                          ],
+                        ),
                       ],
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: FutureBuilder<PackageInfo>(
+                    future: PackageInfo.fromPlatform(),
+                    builder: (context, AsyncSnapshot<PackageInfo> snapshot) => Text(
+                      snapshot.data?.version ?? ' ',
+                      style: const TextStyle(color: Colors.grey, fontSize: 10),
                     ),
-                  ],
-                );
-              },
-            ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
