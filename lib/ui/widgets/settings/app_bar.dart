4c61fc71d0a30ee460cab2d92e334347ae5daf8c
mattpake <pakematt@gmail.com>
2022-01-27 18:30:51 +0100

    refactor

--- lib/ui/widgets/settings/app_bar.dart
+++ lib/ui/widgets/settings/app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loono/constants.dart';
 import 'package:loono/router/app_router.gr.dart';
 
@@ -14,17 +15,21 @@ AppBar settingsAppBar(
     iconTheme: const IconThemeData(color: LoonoColors.black),
     leading: showBackButton
         ? IconButton(
-            icon: const Icon(
-              Icons.arrow_back,
-              size: 32,
+            icon: SizedBox(
+              height: 25,
+              width: 25,
+              child: SvgPicture.asset('assets/icons/arrow_back.svg'),
             ),
-            onPressed: () => Navigator.of(context).pop(),
+            onPressed: () => AutoRouter.of(context).pop(),
           )
         : const SizedBox.shrink(),
     actions: [
-      IconButton(
-        icon: const Icon(Icons.close, size: 37),
-        onPressed: () => AutoRouter.of(context).navigate(closeRoute),
+      Padding(
+        padding: const EdgeInsets.only(right: 15.0),
+        child: IconButton(
+          icon: const Icon(Icons.close, size: 37),
+          onPressed: () => AutoRouter.of(context).navigate(closeRoute),
+        ),
       ),
     ],
   );
