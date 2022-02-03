820ad3714bd2f1947620e3f2c741b6afe9395b6c
Matej Zidek <zidek.matt@gmail.com>
2022-02-03 11:33:30 +0100

    feat: set user token, async button

--- lib/models/firebase_user.dart
+++ lib/models/firebase_user.dart
@@ -1,15 +1,20 @@
-class AuthUser {
-  const AuthUser({
-    required this.uid,
-    this.name,
-    this.email,
-    this.isAnonymous = false,
-    this.avatarUrl,
-  });
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:loono/services/db/database.dart' as db;
 
-  final String uid;
-  final String? name;
-  final String? email;
-  final bool isAnonymous;
-  final String? avatarUrl;
+/// A wrapper for Firebase [User] to avoid conflicts with the local [db.User].
+class AuthUser {
+  const AuthUser({required User firebaseUser}) : _firebaseUser = firebaseUser;
+
+  final User _firebaseUser;
+
+  String get uid => _firebaseUser.uid;
+
+  String? get name => _firebaseUser.displayName;
+
+  String? get email => _firebaseUser.email;
+
+  bool get isAnonymous => _firebaseUser.isAnonymous;
+
+  Future<String> getIdToken({bool forceRefresh = false}) async =>
+      _firebaseUser.getIdToken(forceRefresh);
 }
