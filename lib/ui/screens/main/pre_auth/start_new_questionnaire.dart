8d80c296a57093c1cadb42684054b818ce1af852
Miroslav Peterka <miroslav.peterka1@gmail.com>
2022-02-11 11:08:47 +0100

    První návrh

--- lib/ui/screens/main/pre_auth/start_new_questionnaire.dart
+++ lib/ui/screens/main/pre_auth/start_new_questionnaire.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loono/constants.dart';
-import 'package:loono/helpers/ui_helpers.dart';
 import 'package:loono/l10n/ext.dart';
 import 'package:loono/router/app_router.gr.dart';
 import 'package:loono/ui/widgets/button.dart';
@@ -12,6 +12,7 @@ class StartNewQuestionnaireScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -19,28 +20,42 @@ class StartNewQuestionnaireScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SkipButton(
-                text: context.l10n.already_have_an_account_skip_button,
-                onPressed: () {
-                  if (AutoRouter.of(context).isRouteActive(PreAuthMainRoute().routeName)) {
-                    AutoRouter.of(context).popUntilRoot();
-                  }
-                  AutoRouter.of(context)
-                      .push(PreAuthMainRoute(overridenPreventionRoute: LoginRoute()));
-                },
+              Container(
+                color: const Color.fromRGBO(241, 249, 249, 1),
+                child: Column(
+                  children: [
+                    SkipButton(
+                      text: context.l10n.already_have_an_account_skip_button,
+                      onPressed: () {
+                        if (AutoRouter.of(context).isRouteActive(PreAuthMainRoute().routeName)) {
+                          AutoRouter.of(context).popUntilRoot();
+                        }
+                        AutoRouter.of(context)
+                            .push(PreAuthMainRoute(overridenPreventionRoute: LoginRoute()));
+                      },
+                    ),
+                    const SizedBox(height: 24),
+                    Text(
+                      l10n.carousel_content_3_welcome_hero,
+                      textAlign: TextAlign.start,
+                      style: LoonoFonts.headerFontStyle,
+                    ),
+                    SvgPicture.asset('assets/icons/people.svg'),
+                  ],
+                ),
               ),
-              const SizedBox(height: 24),
-              const Text(
-                'Vítej do společnosti  (ne)obyčejných hrdinů!\n\nTODO: screen UI',
-                textAlign: TextAlign.center,
-                style: LoonoFonts.bigFontStyle,
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 30),
+                child: Column(
+                  children: [
+                    Text(l10n.carousel_content_3_text),
+                    LoonoButton(
+                      text: l10n.start_new_questionnaire_button,
+                      onTap: () => AutoRouter.of(context).push(const OnboardingWrapperRoute()),
+                    ),
+                  ],
+                ),
               ),
-              const Spacer(),
-              LoonoButton(
-                text: context.l10n.start_new_questionnaire_button,
-                onTap: () => AutoRouter.of(context).push(const OnboardingWrapperRoute()),
-              ),
-              SizedBox(height: LoonoSizes.buttonBottomPadding(context)),
             ],
           ),
         ),
