abefc41ae9899846bd321d3d0f6105094553150f
Miroslav Peterka <miroslav.peterka1@gmail.com>
2022-02-11 12:58:27 +0100

    feat: finisfed layout - list for small screens

--- lib/ui/screens/main/pre_auth/start_new_questionnaire.dart
+++ lib/ui/screens/main/pre_auth/start_new_questionnaire.dart
@@ -9,55 +9,57 @@ import 'package:loono/ui/widgets/skip_button.dart';
 
 class StartNewQuestionnaireScreen extends StatelessWidget {
   const StartNewQuestionnaireScreen({Key? key}) : super(key: key);
+  final horizontalPading = 18.0;
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 18, left: 18, right: 18),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: const Color.fromRGBO(241, 249, 249, 1),
-                child: Column(
-                  children: [
-                    SkipButton(
-                      text: context.l10n.already_have_an_account_skip_button,
-                      onPressed: () {
-                        if (AutoRouter.of(context).isRouteActive(PreAuthMainRoute().routeName)) {
-                          AutoRouter.of(context).popUntilRoot();
-                        }
-                        AutoRouter.of(context)
-                            .push(PreAuthMainRoute(overridenPreventionRoute: LoginRoute()));
-                      },
-                    ),
-                    const SizedBox(height: 24),
-                    Text(
+        child: ListView(
+          children: [
+            Container(
+              color: const Color.fromRGBO(241, 249, 249, 1),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  SkipButton(
+                    text: context.l10n.already_have_an_account_skip_button,
+                    onPressed: () {
+                      if (AutoRouter.of(context).isRouteActive(PreAuthMainRoute().routeName)) {
+                        AutoRouter.of(context).popUntilRoot();
+                      }
+                      AutoRouter.of(context)
+                          .push(PreAuthMainRoute(overridenPreventionRoute: LoginRoute()));
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: horizontalPading),
+                    child: Text(
                       l10n.carousel_content_3_welcome_hero,
                       textAlign: TextAlign.start,
                       style: LoonoFonts.headerFontStyle,
                     ),
-                    SvgPicture.asset('assets/icons/people.svg'),
-                  ],
-                ),
+                  ),
+                  SvgPicture.asset('assets/icons/people.svg'),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30, bottom: 30),
-                child: Column(
-                  children: [
-                    Text(l10n.carousel_content_3_text),
-                    LoonoButton(
-                      text: l10n.start_new_questionnaire_button,
-                      onTap: () => AutoRouter.of(context).push(const OnboardingWrapperRoute()),
-                    ),
-                  ],
-                ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: horizontalPading),
+              child: Column(
+                children: [
+                  Text(l10n.carousel_content_3_text, style: LoonoFonts.paragraphFontStyle),
+                  const SizedBox(height: 40),
+                  LoonoButton(
+                    text: l10n.start_new_questionnaire_button,
+                    onTap: () => AutoRouter.of(context).push(const OnboardingWrapperRoute()),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
