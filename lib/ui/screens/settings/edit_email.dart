5a48a6c18f8c820a51a8f3d87c72da204c529a6e
Matej Zidek <zidek.matt@gmail.com>
2022-02-12 10:55:05 +0100

    analyze

--- lib/ui/screens/settings/edit_email.dart
+++ lib/ui/screens/settings/edit_email.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:loono/constants.dart';
 import 'package:loono/helpers/nickname_hint_resolver.dart';
-import 'package:loono/helpers/snackbar_message.dart';
 import 'package:loono/helpers/validators.dart';
 import 'package:loono/l10n/ext.dart';
 import 'package:loono/repositories/user_repository.dart';
