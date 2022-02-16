4638bb927de4609b49cfed835092177f1c97d2ff
Miroslav Peterka <miroslav.peterka1@gmail.com>
2022-02-16 10:58:21 +0100

    feat_loon_430_skip_questionnaire_UI

--- lib/ui/screens/main/pre_auth/continue_onboarding_form.dart
+++ lib/ui/screens/main/pre_auth/continue_onboarding_form.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loono/constants.dart';
-import 'package:loono/helpers/ui_helpers.dart';
 import 'package:loono/l10n/ext.dart';
 import 'package:loono/router/app_router.gr.dart';
 import 'package:loono/ui/widgets/button.dart';
@@ -9,42 +9,80 @@ import 'package:loono/ui/widgets/skip_button.dart';
 
 class ContinueOnboardingFormScreen extends StatelessWidget {
   const ContinueOnboardingFormScreen({Key? key}) : super(key: key);
+  final horizontalPadding = 18.0;
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 18, left: 18, right: 18),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SkipButton(
-                text: context.l10n.already_have_an_account_skip_button,
-                onPressed: () {
-                  if (AutoRouter.of(context).isRouteActive(PreAuthMainRoute().routeName)) {
-                    AutoRouter.of(context).popUntilRoot();
-                  }
-                  AutoRouter.of(context).replaceAll([
-                    LoginRoute(),
-                    PreAuthMainRoute(overridenPreventionRoute: LoginRoute()),
-                  ]);
-                },
+        child: ListView(
+          children: [
+            Container(
+              color: const Color.fromRGBO(241, 249, 249, 1),
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Column(
+                children: [
+                  SkipButton(
+                    text: l10n.already_have_an_account_skip_button,
+                    onPressed: () {
+                      if (AutoRouter.of(context).isRouteActive(PreAuthMainRoute().routeName)) {
+                        AutoRouter.of(context).popUntilRoot();
+                      }
+                      AutoRouter.of(context)
+                          .push(PreAuthMainRoute(overridenPreventionRoute: LoginRoute()));
+                    },
+                  ),
+                  Text(
+                    l10n.continue_onboarding_title,
+                    style: const TextStyle(fontSize: 30),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const CircularProgressIndicator(
+                            color: LoonoColors.red,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            l10n.continue_onboarding_form_button,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: LoonoColors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      SvgPicture.asset(
+                        'assets/icons/a_doctor.svg',
+                        alignment: Alignment.bottomRight,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              const SizedBox(height: 24),
-              const Text(
-                'Schází dokončit jen pár otázek na tvojí cestě za prevencí\n\nTODO: screen UI',
-                textAlign: TextAlign.center,
-                style: LoonoFonts.bigFontStyle,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    l10n.continue_onboarding_text,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 80),
+                  LoonoButton(
+                    text: l10n.continue_onboarding_form_button,
+                    onTap: () => AutoRouter.of(context).push(const OnboardingWrapperRoute()),
+                  ),
+                ],
               ),
-              const Spacer(),
-              LoonoButton(
-                text: context.l10n.continue_onboarding_form_button,
-                onTap: () => AutoRouter.of(context).push(const OnboardingWrapperRoute()),
-              ),
-              SizedBox(height: LoonoSizes.buttonBottomPadding(context)),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
