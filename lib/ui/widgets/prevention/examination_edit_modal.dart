32052bfccd8b4df636e2fade7d332d623ef52918
Matěj Žídek <zidek.matt@gmail.com>
2022-03-02 16:22:16 +0100

    fix(prevention): main screen card statuses (#151)

--- lib/ui/widgets/prevention/examination_edit_modal.dart
+++ lib/ui/widgets/prevention/examination_edit_modal.dart
@@ -45,7 +45,7 @@ void showEditModal(BuildContext pageContext, CategorizedExamination examination)
     );
   }
 
-  final formattedDate = DateFormat('d. MMMM yyyy, kk:mm', 'cs-CZ')
+  final formattedDate = DateFormat('d. MMMM yyyy, hh:mm', 'cs-CZ')
       .format(examination.examination.plannedDate!.toLocal());
 
   showCupertinoModalPopup<void>(
