6cce3965c0c6bfd0dcbf20c57dc3c0949a7611af
Matej Zidek <zidek.matt@gmail.com>
2022-03-02 15:58:05 +0100

    fix(prevention): main screen card statuses

--- lib/helpers/examination_extensions.dart
+++ lib/helpers/examination_extensions.dart
@@ -14,10 +14,9 @@ extension ExaminationPreventionStatusExt on ExaminationPreventionStatus {
     final now = dateTimeNow ?? DateTime.now();
 
     // STATUS: waiting or newToSchedule
-    if (plannedDate == null && lastConfirmedDate != null) {
-      final lastVisitDateWithoutDay = lastConfirmedDate!;
-      final lastVisitDateTime =
-          DateTime(lastVisitDateWithoutDay.year, lastVisitDateWithoutDay.month);
+    if (state == ExaminationStatus.CONFIRMED && lastConfirmedDate != null) {
+      final lastVisitDateTime = lastConfirmedDate!.toLocal();
+      final lastVisitDateWithoutDay = DateTime(lastVisitDateTime.year, lastVisitDateTime.month);
 
       // if last visit date is before: CURRENT_MONTH - (INTERVAL - 2 months)
       final subtractedWaitingDate = DateTime(
@@ -25,17 +24,21 @@ extension ExaminationPreventionStatusExt on ExaminationPreventionStatus {
         now.month - (intervalYears * MONTHS_IN_YEAR - TO_SCHEDULE_MONTHS_TRANSFER),
       );
       // then waiting time has ended, move to "TO BE SCHEDULED"
-      if (lastVisitDateTime.isBefore(subtractedWaitingDate) ||
-          lastVisitDateTime.isAtSameMomentAs(subtractedWaitingDate)) {
+      if (lastVisitDateWithoutDay.isBefore(subtractedWaitingDate) ||
+          lastVisitDateWithoutDay.isAtSameMomentAs(subtractedWaitingDate)) {
         return const ExaminationCategory.newToSchedule();
       }
       // else wait
       return const ExaminationCategory.waiting();
     }
 
+    if (state == ExaminationStatus.UNKNOWN) {
+      return const ExaminationCategory.newToSchedule();
+    }
+
     // STATUS: scheduled or scheduledSoonOrOverdue
     if (plannedDate != null) {
-      final diffInDaysFromNow = plannedDate!.difference(now).inDays;
+      final diffInDaysFromNow = plannedDate!.toLocal().difference(now).inDays;
       // if it is more than 30 days before the check-up visit the status is "SCHEDULED"
       if (diffInDaysFromNow > 30) return const ExaminationCategory.scheduled();
       // else 30 days or equal before check-up visit the status is "SCHEDULED_SOON_OR_OVERDUE"
