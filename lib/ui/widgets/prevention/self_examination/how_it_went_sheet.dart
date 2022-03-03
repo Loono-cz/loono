bf487d3b31e9262f844a37b7811923037ffa9532
Matěj Žídek <zidek.matt@gmail.com>
2022-03-03 19:03:29 +0100

    feat(LOON-492): extend statuses for self examination cards (#140)
    * extend statuses for self examination cards
    
    * use latest api
    
    * fixes
    
    * send correct status

--- lib/ui/widgets/prevention/self_examination/how_it_went_sheet.dart
+++ lib/ui/widgets/prevention/self_examination/how_it_went_sheet.dart
@@ -2,9 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:loono/constants.dart';
 import 'package:loono/router/app_router.gr.dart';
+import 'package:loono/services/api_service.dart';
+import 'package:loono/services/examinations_service.dart';
 import 'package:loono/ui/widgets/button.dart';
 import 'package:loono/ui/widgets/how/loon_botton_sheet.dart';
+import 'package:loono/utils/registry.dart';
 import 'package:loono_api/loono_api.dart';
+import 'package:provider/provider.dart';
 
 void showHowItWentSheet(BuildContext context, Sex sex, int points) {
   showModalBottomSheet<void>(
@@ -24,14 +28,32 @@ void showHowItWentSheet(BuildContext context, Sex sex, int points) {
             const SizedBox(height: 60),
             LoonoButton.light(
               text: 'Je to v pořádku',
-              onTap: () {
-                AutoRouter.of(context).push(NoFindingRoute(points: points));
+              onTap: () async {
+                // TODO: temporary call for debug
+                await registry.get<ApiService>().confirmSelfExamination(
+                  SelfExaminationType.TESTICULAR,
+                  result: SelfExaminationResult((b) {
+                    b.result = SelfExaminationResultResultEnum.OK;
+                  }),
+                );
+                await Provider.of<ExaminationsProvider>(context, listen: false).fetchExaminations();
+                await AutoRouter.of(context).push(NoFindingRoute(points: points));
               },
             ),
             const SizedBox(height: 20),
             LoonoButton.light(
               text: 'Něco jsem našla',
-              onTap: () => AutoRouter.of(context).push(HasFindingRoute(sex: sex)),
+              onTap: () async {
+                // TODO: temporary call for debug
+                await registry.get<ApiService>().confirmSelfExamination(
+                  SelfExaminationType.TESTICULAR,
+                  result: SelfExaminationResult((b) {
+                    b.result = SelfExaminationResultResultEnum.FINDING;
+                  }),
+                );
+                await Provider.of<ExaminationsProvider>(context, listen: false).fetchExaminations();
+                await AutoRouter.of(context).push(HasFindingRoute(sex: sex));
+              },
             ),
           ],
         ),
