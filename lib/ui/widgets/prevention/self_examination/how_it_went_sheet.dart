2597d8c39e733988888cd49f05e41f493b5fe8e4
Matej Zidek <zidek.matt@gmail.com>
2022-03-03 15:10:25 +0100

    fixes

--- lib/ui/widgets/prevention/self_examination/how_it_went_sheet.dart
+++ lib/ui/widgets/prevention/self_examination/how_it_went_sheet.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:loono/constants.dart';
 import 'package:loono/router/app_router.gr.dart';
 import 'package:loono/services/api_service.dart';
+import 'package:loono/services/examinations_service.dart';
 import 'package:loono/ui/widgets/button.dart';
 import 'package:loono/ui/widgets/how/loon_botton_sheet.dart';
 import 'package:loono/utils/registry.dart';
 import 'package:loono_api/loono_api.dart';
+import 'package:provider/provider.dart';
 
 void showHowItWentSheet(BuildContext context, Sex sex) {
   showModalBottomSheet<void>(
@@ -29,18 +31,29 @@ void showHowItWentSheet(BuildContext context, Sex sex) {
               onTap: () async {
                 // TODO: temporary call for debug
                 await registry.get<ApiService>().confirmSelfExamination(
-                      SelfExaminationType.TESTICULAR,
-                      result: SelfExaminationResult((b) {
-                        b.result = SelfExaminationResultResultEnum.OK;
-                      }),
-                    );
+                  SelfExaminationType.TESTICULAR,
+                  result: SelfExaminationResult((b) {
+                    b.result = SelfExaminationResultResultEnum.OK;
+                  }),
+                );
+                await Provider.of<ExaminationsProvider>(context, listen: false).fetchExaminations();
                 await AutoRouter.of(context).push(const NoFindingRoute());
               },
             ),
             const SizedBox(height: 20),
             LoonoButton.light(
               text: 'Něco jsem našla',
-              onTap: () => AutoRouter.of(context).push(HasFindingRoute(sex: sex)),
+              onTap: () async {
+                // TODO: temporary call for debug
+                await registry.get<ApiService>().confirmSelfExamination(
+                  SelfExaminationType.TESTICULAR,
+                  result: SelfExaminationResult((b) {
+                    b.result = SelfExaminationResultResultEnum.NOT_OK;
+                  }),
+                );
+                await Provider.of<ExaminationsProvider>(context, listen: false).fetchExaminations();
+                await AutoRouter.of(context).push(HasFindingRoute(sex: sex));
+              },
             ),
           ],
         ),
