7af15701e86296849fbf3b8d0312c0a0b4a779bb
Martin Macek <martin.macek@blackpoint.dev>
2022-03-09 22:26:01 +0100

    feat: use hybrid composition on new androids

--- lib/helpers/map_variables.dart
+++ lib/helpers/map_variables.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: constant_identifier_names
+import 'dart:io';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:loono/utils/app_config.dart';
+import 'package:loono/utils/registry.dart';
 
 class MapVariables {
   /// Prague, main train station
@@ -8,4 +12,15 @@ class MapVariables {
   static const double DEFAULT_ZOOM = 13.0;
 
   static const double DOCTOR_DETAIL_ZOOM = 17.0;
+}
+
+bool useHybridComposition() {
+  final platform = registry.get<AppConfig>().platformVersion;
+  if (Platform.isAndroid && platform.contains('Android ')) {
+    final version = int.parse(platform.replaceAll('Android ', ''));
+
+    /// use hybrid composition for android api level >= 29 (android 10 +)
+    return version >= 29;
+  }
+  return false;
 }
