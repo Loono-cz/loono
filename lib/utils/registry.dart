9f3741db099dfff2ba8160cd19fce7c32666c5d9
Martin Macek <martin.macek@blackpoint.dev>
2022-03-12 17:20:57 +0100

    fix: disable http cert in tests

--- lib/utils/registry.dart
+++ lib/utils/registry.dart
@@ -82,10 +82,12 @@ Future<void> setup({
   final dio = dioOverride ?? Dio(defaultDioOptions);
 
   /// Ignore api certificate - remove this asap when server has set
-  (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (HttpClient client) {
-    client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-    return client;
-  };
+  if (dioOverride == null) {
+    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (HttpClient client) {
+      client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+      return client;
+    };
+  }
   // remove end
 
   if (dioOverride == null) {
