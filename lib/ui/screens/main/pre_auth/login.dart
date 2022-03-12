44bb1f3aaa9ad8d20e7d3f1487fb391baeb8cea4
Martin Macek <martin.macek@blackpoint.dev>
2022-03-12 15:26:55 +0100

    feat: api switch

--- lib/ui/screens/main/pre_auth/login.dart
+++ lib/ui/screens/main/pre_auth/login.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' as s;
 import 'package:flutter_svg/svg.dart';
 import 'package:loono/constants.dart';
 import 'package:loono/helpers/onboarding_state_helpers.dart';
@@ -12,6 +14,7 @@ import 'package:loono/services/auth/failures.dart';
 import 'package:loono/services/database_service.dart';
 import 'package:loono/ui/widgets/social_login_button.dart';
 import 'package:loono/utils/registry.dart';
+import 'package:yaml/yaml.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
@@ -28,6 +31,41 @@ class LoginScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Spacer(flex: 4),
+            if (kDebugMode)
+              TextButton(
+                onPressed: () async {
+                  final data = await s.rootBundle.loadString('assets/supported_apis.yaml');
+                  final apis = loadYaml(data) as YamlList;
+
+                  await showDialog<void>(
+                    context: context,
+                    barrierDismissible: false, // user must tap button!
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Select API'),
+                        content: SingleChildScrollView(
+                          child: ListBody(
+                            children: apis.nodes
+                                .map(
+                                  (dynamic api) => ElevatedButton(
+                                    onPressed: () async {
+                                      await registry
+                                          .get<AuthService>()
+                                          .switchApi(api['url'].toString());
+                                      await AutoRouter.of(context).pop();
+                                    },
+                                    child: Text(api['url'].toString()),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: const Text('switch api'),
+              ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 18.0),
               child: Align(
