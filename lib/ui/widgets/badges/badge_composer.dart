4f68cfcc82f5edfc8d030e6b6694dc3661c8e5aa
ulcdenis <93252999+ulcdenis@users.noreply.github.com>
2022-03-15 21:41:31 +0100

    Ulcdenis/loon 531 badges description (#176)
    * belt desc
    
    * add other badges desc
    
    * edit paddings
    
    * localization
    
    * formatting
    
    * show description bool
    
    * formatting

--- lib/ui/widgets/badges/badge_composer.dart
+++ lib/ui/widgets/badges/badge_composer.dart
@@ -2,6 +2,7 @@ import 'package:built_collection/built_collection.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:loono/l10n/ext.dart';
 import 'package:loono/services/database_service.dart';
 import 'package:loono/services/db/database.dart';
 import 'package:loono/utils/registry.dart';
@@ -10,9 +11,11 @@ import 'package:loono_api/loono_api.dart';
 class BadgeComposer extends StatefulWidget {
   const BadgeComposer({
     Key? key,
+    this.showDescription = false,
     this.topPadding = 40,
   }) : super(key: key);
 
+  final bool showDescription;
   final double topPadding;
 
   @override
@@ -26,72 +29,332 @@ class _BadgeComposerState extends State<BadgeComposer> {
 
   Widget _getGoogles(Sex sex, int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/goggles-${sex == Sex.MALE ? 'man' : 'woman'}/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/goggles-${sex == Sex.MALE ? 'man' : 'woman'}/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    top: 50,
+                    left: (MediaQuery.of(context).size.width / 2) - 150,
+                    child: Column(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 110),
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            context.l10n.badge_googles_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          'assets/badges/lines/googles_line.svg',
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getHeadband(int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/headband/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/headband/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    top: 30,
+                    left: (MediaQuery.of(context).size.width / 2) + 30,
+                    child: Column(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 120),
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                            context.l10n.badge_headband_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          'assets/badges/lines/headband_line.svg',
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getBoots(int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/boots/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/boots/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    left: (MediaQuery.of(context).size.width / 2) - 170,
+                    top: 260,
+                    child: Column(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 130),
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            context.l10n.badge_boots_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          'assets/badges/lines/boots_line.svg',
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getBelt(Sex sex, int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/belt-${sex == Sex.MALE ? 'man' : 'woman'}/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/belt-${sex == Sex.MALE ? 'man' : 'woman'}/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    left: (MediaQuery.of(context).size.width / 2) + 40,
+                    child: Column(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 100),
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                            sex == Sex.FEMALE
+                                ? context.l10n.badge_belt_female_desc.toUpperCase()
+                                : context.l10n.badge_belt_male_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          'assets/badges/lines/belt_line.svg',
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getArmour(int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/armour-woman/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/armour-woman/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    top: 90,
+                    left: (MediaQuery.of(context).size.width / 2) - 160,
+                    child: Column(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 130),
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            context.l10n.badge_buckle_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          'assets/badges/lines/buckle_line.svg',
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getCloak(int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/cloak/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/cloak/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    top: 240,
+                    left: (MediaQuery.of(context).size.width / 2) + 30,
+                    child: Stack(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 150),
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            context.l10n.badge_cloak_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          'assets/badges/lines/cloak_line.svg',
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getCloakBuckle(int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/cloak-buckle/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: SvgPicture.asset(
+              'assets/badges/cloak-buckle/level_$level.svg',
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getGloves(Sex sex, int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/gloves-${sex == Sex.MALE ? 'man' : 'woman'}/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/gloves-${sex == Sex.MALE ? 'man' : 'woman'}/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    top: 180,
+                    left: (MediaQuery.of(context).size.width / 2) + 40,
+                    child: Stack(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 120),
+                          height: 35,
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            context.l10n.badge_gloves_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 20,
+                          child: SvgPicture.asset(
+                            'assets/badges/lines/gloves_line.svg',
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
 
   Widget _getShield(int level) {
     return level > 0
-        ? SvgPicture.asset(
-            'assets/badges/shield/level_$level.svg',
+        ? SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/badges/shield/level_$level.svg',
+                ),
+                if (widget.showDescription)
+                  Positioned(
+                    top: 210,
+                    left: (MediaQuery.of(context).size.width / 2) - 180,
+                    child: Stack(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxWidth: 120),
+                          height: 35,
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            context.l10n.badge_shield_desc.toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 20,
+                          child: SvgPicture.asset(
+                            'assets/badges/lines/shield_line.svg',
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           )
         : const SizedBox();
   }
@@ -124,10 +387,11 @@ class _BadgeComposerState extends State<BadgeComposer> {
                 }
 
                 return SizedBox(
-                  width: 180,
+                  width: MediaQuery.of(context).size.width,
                   height: 300,
                   child: sex != null
                       ? Stack(
+                          alignment: Alignment.center,
                           children: [
                             _getCloak(_levelOf(BadgeType.COAT)),
                             SvgPicture.asset(
