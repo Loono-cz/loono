70cf79999291720a6f866bdc2a0613b9dccba101
Matěj Žídek <zidek.matt@gmail.com>
2022-03-29 08:49:02 +0200

    feat(auth): store securely apple account info on the first authorization (#194)

--- lib/services/auth/failures.dart
+++ lib/services/auth/failures.dart
@@ -5,13 +5,19 @@ import 'package:loono/models/social_login_account.dart';
 
 part 'failures.freezed.dart';
 
+const _anonymizedAppleEmailDomain = 'privaterelay.appleid.com';
+
 extension AuthFailureMessageExt on AuthFailure {
   String getMessage(BuildContext context) {
     return when(
       unknown: (message) => message ?? context.l10n.auth_unknown_failure_message,
       noMessage: () => '',
       accountNotExists: (socialAccount) {
-        final email = socialAccount.email;
+        var email = socialAccount.email;
+        if (email?.endsWith(_anonymizedAppleEmailDomain) == true) {
+          // this email with such domain will not say to the user anything
+          email = null;
+        }
         return '${context.l10n.auth_account_not_exists_message} ${email == null ? '' : '($email).'}';
       },
       network: (message) => message ?? context.l10n.auth_network_failure_message,
--- lib/utils/app_clear.dart
+++ lib/utils/app_clear.dart
@@ -2,13 +2,17 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:loono/services/auth/auth_service.dart';
 import 'package:loono/services/database_service.dart';
 import 'package:loono/services/notification_service.dart';
+import 'package:loono/services/secure_storage_service.dart';
 import 'package:loono/utils/registry.dart';
 
 Future<void> appClear({bool deletingAccount = false}) async {
   await registry.get<AuthService>().signOut();
   await registry.get<DatabaseService>().clearDb();
+  await registry.get<SecureStorageService>().deleteAll();
+
   // clears saved user avatar and other app's temp data
   await registry.get<DefaultCacheManager>().emptyCache();
+
   if (deletingAccount) {
     await registry.get<NotificationService>().removeId();
   }
