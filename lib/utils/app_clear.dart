6f89c0a3f465bfaaddfafc1b33be059fd3742086
Matej Zidek <zidek.matt@gmail.com>
2022-04-06 01:30:30 +0200

    feat: delete user files on delete account

--- lib/utils/app_clear.dart
+++ lib/utils/app_clear.dart
@@ -1,17 +1,20 @@
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:loono/services/auth/auth_service.dart';
 import 'package:loono/services/database_service.dart';
+import 'package:loono/services/firebase_storage_service.dart';
 import 'package:loono/services/notification_service.dart';
 import 'package:loono/services/secure_storage_service.dart';
 import 'package:loono/utils/registry.dart';
 
 Future<void> appClear({bool deletingAccount = false}) async {
+  if (deletingAccount) {
+    await registry.get<FirebaseStorageService>().deleteAll();
+  }
   await registry.get<AuthService>().signOut();
   await registry.get<DatabaseService>().clearDb();
   await registry.get<SecureStorageService>().deleteAll();
+  await registry.get<NotificationService>().removeUserId();
 
   // clears saved user avatar and other app's temp data
   await registry.get<DefaultCacheManager>().emptyCache();
-
-  await registry.get<NotificationService>().removeUserId();
 }
