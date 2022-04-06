3d6d8897d2111cebefb77b2195ca5d07673428e6
Matěj Žídek <zidek.matt@gmail.com>
2022-04-06 22:04:01 +0200

    feat(firebase): more setups, delete user files on delete account (#213)
    * feat: enable crashlytics in non-debug mode
    
    * feat: disable analytics in debug mode
    
    * update readme
    
    * feat: delete user files on delete account

--- lib/utils/registry.dart
+++ lib/utils/registry.dart
@@ -10,6 +10,8 @@ import 'package:dio_smart_retry/dio_smart_retry.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -55,6 +57,7 @@ Future<void> setup({
 }) async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
 
   final appInfo = await PackageInfo.fromPlatform();
   await dotenv.load(fileName: 'assets/.env');
@@ -157,6 +160,7 @@ Future<void> setup({
   );
 
   registry.registerSingleton<FirebaseAnalytics>(FirebaseAnalytics.instance);
+  await registry.get<FirebaseAnalytics>().setAnalyticsCollectionEnabled(!kDebugMode);
 
   registry.registerSingleton<LoonoApi>(LoonoApi(dio: dio));
 
