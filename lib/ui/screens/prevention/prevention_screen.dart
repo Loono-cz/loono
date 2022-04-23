714fc164c9bfa0f920ac0a2bd9e113706f531aa7
Matej Zidek <zidek.matt@gmail.com>
2022-04-23 16:51:03 +0200

    feat(LOON-618): show/hide badge overview on click

--- lib/ui/screens/prevention/prevention_screen.dart
+++ lib/ui/screens/prevention/prevention_screen.dart
@@ -36,17 +36,19 @@ class PreventionScreen extends StatelessWidget {
                   padding: EdgeInsets.only(top: 40),
                   child: BadgeComposer(showDescription: false),
                 ),
-                ValueListenableBuilder(
-                  valueListenable: extentFromTop,
-                  builder: (context, double? value, child) {
-                    if (value != null) {
-                      return AvatarBubbleArrow(
-                        extent: value,
-                        height: constraints.maxHeight,
-                      );
-                    }
-                    return const SizedBox();
-                  },
+                IgnorePointer(
+                  child: ValueListenableBuilder(
+                    valueListenable: extentFromTop,
+                    builder: (context, double? value, child) {
+                      if (value != null) {
+                        return AvatarBubbleArrow(
+                          extent: value,
+                          height: constraints.maxHeight,
+                        );
+                      }
+                      return const SizedBox();
+                    },
+                  ),
                 ),
                 ExaminationsSheetOverlay(
                   convertExtent: convertExtent,
