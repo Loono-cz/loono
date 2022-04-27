9a512006034d50ecf3ce4d398fa7e91c083a9e85
Matej Zidek <zidek.matt@gmail.com>
2022-04-27 23:22:08 +0200

    fix(LOON-615): add scrollbar to calendar list

--- lib/ui/widgets/scrollbar.dart
+++ lib/ui/widgets/scrollbar.dart
@@ -5,17 +5,21 @@ class LoonoScrollbar extends StatelessWidget {
   const LoonoScrollbar({
     Key? key,
     required this.child,
+    this.thumbColor,
+    this.trackColor,
   }) : super(key: key);
 
   final Widget child;
+  final Color? thumbColor;
+  final Color? trackColor;
 
   @override
   Widget build(BuildContext context) {
     return Theme(
       data: Theme.of(context).copyWith(
         scrollbarTheme: Theme.of(context).scrollbarTheme.copyWith(
-              thumbColor: MaterialStateProperty.all(LoonoColors.primaryWashed),
-              trackColor: MaterialStateProperty.all(LoonoColors.beigeLighter),
+              thumbColor: MaterialStateProperty.all(thumbColor ?? LoonoColors.primaryWashed),
+              trackColor: MaterialStateProperty.all(trackColor ?? LoonoColors.beigeLighter),
               trackBorderColor: MaterialStateProperty.all(Colors.transparent),
               trackVisibility: MaterialStateProperty.all(true),
             ),
