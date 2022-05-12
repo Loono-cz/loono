4120d6810cee91824026cb6df568286f70acce9e
Matej Zidek <zidek.matt@gmail.com>
2022-05-13 00:50:04 +0200

    test 3

--- bin/ci_workflow_scripts/verify_formatting.sh
+++ bin/ci_workflow_scripts/verify_formatting.sh
@@ -2,9 +2,9 @@
 
 libFiles=$(find ./lib/ -type f -not -regex ".*\.\(freezed\|gr\|g\).dart" -not -name "*.arb")
 for file in $libFiles; do
-  dart format --line-length 100 --output=none --set-exit-if-changed $file || exitCode=$?
-  if [ $exitCode == 1 ]; then
-      exit 1;
+  dart format --line-length 100 --output=none --set-exit-if-changed $file
+  if [ $? -eq 1 ]; then
+    exit 1
   fi
 done
 
