b3639002de579250ca4be0d0f855b546181a2332
Petr Noga <n.skypo@gmail.com>
2022-10-02 23:54:33 +0200

    Fixing buttons not showing.

--- lib/ui/screens/prevention/examination_detail/examination_detail.dart
+++ lib/ui/screens/prevention/examination_detail/examination_detail.dart
@@ -379,10 +379,11 @@ class _ExaminationDetailState extends State<ExaminationDetail> {
         children: [
           // displays calendar button for the scheduled check-ups which did not happen yet
           if (_nextVisitDate != null &&
-              [
-                const ExaminationCategory.scheduled(),
-                const ExaminationCategory.scheduledSoonOrOverdue()
-              ].contains(widget.categorizedExamination.category)) ...[
+                  [
+                    const ExaminationCategory.scheduled(),
+                    const ExaminationCategory.scheduledSoonOrOverdue()
+                  ].contains(widget.categorizedExamination.category) ||
+              !_isPeriodicalExam) ...[
             StreamBuilder<CalendarEvent?>(
               stream: _calendarEventsDao.watch(_examinationType),
               builder: (streamContext, snapshot) {
@@ -480,8 +481,7 @@ class _ExaminationDetailState extends State<ExaminationDetail> {
                 const ExaminationCategory.newToSchedule(),
                 const ExaminationCategory.waiting()
               ].contains(widget.categorizedExamination.category) &&
-              !_isPeriodicalExam &&
-              _examinationCategoryType != ExaminationCategoryType.MANDATORY) ...[
+              _isPeriodicalExam) ...[
             Expanded(
               child: LoonoButton(
                 key: const Key('examinationDetailPage_btn_order'),
