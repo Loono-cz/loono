72959e4e5d9ecd84635b231eb7f98658a30bdc51
Brambora0 <makyskorepa@gmail.com>
2022-10-15 11:52:16 +0200

    fix/prohlidky_se_radi_spatne (#434) oprava řadění prohlídek (https://github.com/Loono-cz/loono-tasks/issues/112)

---
+++ lib/helpers/examination_category_types.dart
@@ -0,0 +1,83 @@
+import 'package:loono/helpers/examination_extensions.dart';
+import 'package:loono/helpers/examination_types.dart';
+import 'package:loono/models/categorized_examination.dart';
+import 'package:loono_api/loono_api.dart';
+
+extension ExaminationCategoryTypeExt on ExaminationCategoryType {
+  int compareTo(ExaminationCategoryType? other) {
+    if (other == null) return 1;
+    return _toInt().compareTo(other._toInt());
+  }
+
+  int _toInt() {
+    switch (this) {
+      case ExaminationCategoryType.CUSTOM:
+        return 1;
+      case ExaminationCategoryType.MANDATORY:
+        return 0;
+    }
+    return -1;
+  }
+}
+
+int compareExaminationType(
+  CategorizedExamination a,
+  CategorizedExamination b, {
+  bool compareByDate = true,
+  bool repeat = true,
+}) {
+  final aType = a.examination.examinationCategoryType;
+  final bType = b.examination.examinationCategoryType;
+  final comparedCategoryType = aType?.compareTo(bType) ?? 0;
+  if (comparedCategoryType == 0) {
+    switch (aType) {
+      case ExaminationCategoryType.CUSTOM:
+        final compared = compareByDate
+            ? _compareExamDate(a, b)
+            : a.examination.examinationType.l10n_name.compareTo(
+                b.examination.examinationType.l10n_name,
+              );
+        return _processComparedValue(
+            a: a,
+            b: b,
+            compared: compared,
+            compareByDate: compareByDate,
+            tryAgainOnEquality: repeat,
+        );
+      case ExaminationCategoryType.MANDATORY:
+        final compared = compareByDate
+            ? _compareExamDate(a, b)
+            : a.examination.priority.compareTo(b.examination.priority);
+        return _processComparedValue(
+            a: a,
+            b: b,
+            compared: compared,
+            compareByDate: compareByDate,
+            tryAgainOnEquality: repeat,
+        );
+    }
+  }
+  return 0;
+}
+
+int _compareExamDate(CategorizedExamination a, CategorizedExamination b) {
+  final duration = a.examination.targetExamDate?.difference(b.examination.targetExamDate!).inDays ?? 0;
+  return duration > 0 ? 1 : duration < 0 ? -1 : 0;
+}
+
+int _processComparedValue({
+  required CategorizedExamination a,
+  required CategorizedExamination b,
+  required int compared,
+  required bool compareByDate,
+  required bool tryAgainOnEquality,
+}) {
+  return compared == 0 && tryAgainOnEquality
+      ? compareExaminationType(
+          a,
+          b,
+          compareByDate: !compareByDate,
+          repeat: false,
+        )
+      : compared;
+}
