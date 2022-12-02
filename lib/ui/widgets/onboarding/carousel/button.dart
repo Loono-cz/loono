f28eb2f13c3fdfa8912a604c39f92048568224fb
Jakub Smolík <jakub.smolik@cgi.com>
2022-12-02 16:21:25 +0100

    #141-fix: gh actions format

--- lib/ui/widgets/onboarding/carousel/button.dart
+++ lib/ui/widgets/onboarding/carousel/button.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loono/ui/widgets/button.dart';
 
 class CarouselButton extends StatelessWidget {
-  const CarouselButton({Key? key, required this.text, this.onTap})
-      : super(key: key);
+  const CarouselButton({Key? key, required this.text, this.onTap}) : super(key: key);
 
   final String text;
   final VoidCallback? onTap;
@@ -11,9 +10,9 @@ class CarouselButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.symmetric(horizontal: 18.0),
-        child: LoonoButton(onTap: onTap, text: text),
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.symmetric(horizontal: 18.0),
+      child: LoonoButton(onTap: onTap, text: text),
     );
   }
 }
