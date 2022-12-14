fdaec1d66a4c1f1b458664b282982120195be9ad
Brambora0 <makyskorepa@gmail.com>
2022-12-14 20:19:10 +0100

    routing_push_notifikaci#460

--- lib/ui/widgets/notification_loading_widget.dart
+++ lib/ui/widgets/notification_loading_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:loono/constants.dart';
-import 'package:loono/l10n/ext.dart';
 import 'package:loono/router/notification_router.dart';
 import 'package:loono/ui/widgets/space.dart';
 
@@ -22,18 +21,6 @@ class NotificationLoadingWidget extends StatelessWidget {
               children: [
                 SvgPicture.asset(
                   LoonoAssets.welcomeLogo,
-                ),
-                const CustomSpacer.vertical(40),
-                Text(
-                  context.l10n.notification_loading_header,
-                  style: LoonoFonts.headerFontStyle,
-                ),
-                const CustomSpacer.vertical(15),
-                Text(
-                  screen == NotificationScreen.examination
-                      ? context.l10n.notification_loading_examination
-                      : context.l10n.notification_loading_self_examination,
-                  style: LoonoFonts.subtitleFontStyle,
                 ),
                 const CustomSpacer.vertical(40),
                 const CircularProgressIndicator(
