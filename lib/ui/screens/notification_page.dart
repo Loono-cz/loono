ab1a9ba67ec0108081b79521359a5cedcd8381df
magdalenamajakova <magdalena.majakova@gmail.com>
2022-12-29 14:50:38 +0100

    newsletter image

---
+++ lib/ui/screens/notification_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:loono/constants.dart';
+import 'package:loono/l10n/ext.dart';
+import 'package:loono/ui/screens/settings/settings_bottom_sheet.dart';
+import 'package:loono/ui/widgets/button.dart';
+import 'package:loono/ui/widgets/close_button.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+enum NotficationType { newsletter, donate }
+
+class NotificationPage extends StatefulWidget {
+  const NotificationPage({Key? key, required this.notficationType}) : super(key: key);
+  final NotficationType notficationType;
+  @override
+  NotificationPageState createState() => NotificationPageState();
+}
+
+class NotificationPageState extends State<NotificationPage> {
+  Future<void> _closeForm(BuildContext context) async => Navigator.of(context).pop();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: LoonoCloseButton(onPressed: () async => _closeForm(context)),
+          ),
+          if (widget.notficationType == NotficationType.donate)
+            SvgPicture.asset('assets/donate_ilustration.svg')
+          else if (widget.notficationType == NotficationType.newsletter)
+            SvgPicture.asset('assets/newsletter_illustration.svg'),
+          Text(
+            context.l10n.do_you_like_loono,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 24,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          Text(
+            context.l10n.donate_desc_loono,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          ///button
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: LoonoButton(
+                text: context.l10n.donate_label_btn,
+                onTap: () async {
+                  if (await canLaunchUrlString(LoonoStrings.donateUrl)) {
+                    await launchUrlString(LoonoStrings.donateUrl);
+                  }
+                },
+              ),
+            ),
+          ),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Colors.black87,
+              ),
+              children: [
+                TextSpan(
+                  text: context.l10n.donate_anytime,
+                ),
+                const TextSpan(text: '\n\n'),
+                TextSpan(
+                  text: context.l10n.donate_info_notification,
+                ),
+                TextSpan(
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => showSettingsSheet(
+                          context,
+                          settingsPage: SettingsPage.SettingsEditPage,
+                          expand: true,
+                        ),
+                  text: context.l10n.notification_settings_edit,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
