d36bda30cb8c95cc21f0630f8977e50dfae4f569
magdalenamajakova <magdalena.majakova@gmail.com>
2022-12-29 19:27:43 +0100

    text in widgets is selected according to a notfiication type

--- lib/ui/screens/notification_page.dart
+++ lib/ui/screens/notification_page.dart
@@ -36,13 +36,17 @@ class NotificationPageState extends State<NotificationPage> {
           else if (widget.notficationType == NotficationType.newsletter)
             SvgPicture.asset('assets/newsletter_illustration.svg'),
           Text(
-            context.l10n.do_you_like_loono,
+            (widget.notficationType == NotficationType.donate)
+                ? context.l10n.do_you_like_loono
+                : context.l10n.newsletter_be_informed,
             style: LoonoFonts.customExamLabel,
             textAlign: TextAlign.center,
           ),
 
           Text(
-            context.l10n.donate_desc_loono,
+            (widget.notficationType == NotficationType.donate)
+                ? context.l10n.donate_desc_loono
+                : context.l10n.newsletter_desc,
             style: LoonoFonts.spinnerTextOnceTo,
             textAlign: TextAlign.center,
           ),
@@ -53,7 +57,9 @@ class NotificationPageState extends State<NotificationPage> {
             child: Padding(
               padding: const EdgeInsets.only(bottom: 8.0),
               child: LoonoButton(
-                text: context.l10n.donate_label_btn,
+                text: (widget.notficationType == NotficationType.donate)
+                    ? context.l10n.donate_label_btn
+                    : context.l10n.newsletter_label_btn,
                 onTap: () async {
                   if (await canLaunchUrlString(LoonoStrings.donateUrl)) {
                     await launchUrlString(LoonoStrings.donateUrl);
@@ -62,31 +68,38 @@ class NotificationPageState extends State<NotificationPage> {
               ),
             ),
           ),
-          RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-              style: LoonoFonts.notificationSubText,
-              children: [
-                TextSpan(
-                  text: context.l10n.donate_anytime,
-                ),
-                const TextSpan(text: '\n\n'),
-                TextSpan(
-                  text: context.l10n.donate_info_notification,
-                ),
-                TextSpan(
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () => showSettingsSheet(
-                          context,
-                          settingsPage: SettingsPage.SettingsEditPage,
-                          expand: true,
-                        ),
-                  text: context.l10n.notification_settings_edit,
-                )
-              ],
+          if (widget.notficationType == NotficationType.donate)
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: LoonoFonts.notificationSubText,
+                children: [
+                  TextSpan(
+                    text: context.l10n.donate_anytime,
+                  ),
+                  const TextSpan(text: '\n\n'),
+                  TextSpan(
+                    text: context.l10n.donate_info_notification,
+                  ),
+                  TextSpan(
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () => showSettingsSheet(
+                            context,
+                            settingsPage: SettingsPage.SettingsEditPage,
+                            expand: true,
+                          ),
+                    text: context.l10n.notification_settings_edit,
+                  )
+                ],
+              ),
+            )
+          else if (widget.notficationType == NotficationType.newsletter)
+            Text(
+              context.l10n.newsletter_edit,
+              style: LoonoFonts.spinnerTextOnceTo,
+              textAlign: TextAlign.center,
             ),
-          ),
         ],
       ),
     );
