9a0390650fe9225c88f6866d0c3e98a6ec805411
Brambora0 <makyskorepa@gmail.com>
2023-01-24 22:20:56 +0100

    fix format

--- lib/helpers/platform_helpers.dart
+++ lib/helpers/platform_helpers.dart
@@ -5,7 +5,7 @@ import 'package:device_info_plus/device_info_plus.dart';
 
 /// Gets Android version sdk int. Returns null when platform is not Android.
 Future<int?> getAndroidVersion() async {
-  if(!Platform.isAndroid) {
+  if (!Platform.isAndroid) {
     return null;
   }
   final android = await DeviceInfoPlugin().androidInfo;
