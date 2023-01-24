2b71544aee57495dcf51055443e393851b603fee
Adam Šupej <adam.supej@cgi.com>
2023-01-24 11:28:24 +0100

    fix: contact_icon_display#158

--- lib/ui/widgets/find_doctor/search_doctor_card.dart
+++ lib/ui/widgets/find_doctor/search_doctor_card.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loono/constants.dart';
 import 'package:loono/helpers/simple_health_care_provider_helper.dart';
+import 'package:loono/l10n/ext.dart';
+import 'package:loono/models/api_response.dart';
+import 'package:loono/services/api_service.dart';
+import 'package:loono/utils/registry.dart';
 import 'package:loono_api/loono_api.dart';
 
 class SearchDoctorCard extends StatelessWidget {
@@ -57,20 +61,59 @@ class SearchDoctorCard extends StatelessWidget {
                               maxLines: 3,
                             ),
                           ),
-                        // TODO: display icon if they have contact
-                        // ignore: dead_code
-                        if (false) ...[
-                          const SizedBox(width: 8),
-                          SvgPicture.asset(
-                            'assets/icons/telephone.svg',
-                            color: LoonoColors.grey,
+                        FutureBuilder(
+                          future: registry.get<ApiService>().postProviderDetail(
+                            providersIds: [
+                              HealthcareProviderId((p) {
+                                p
+                                  ..institutionId = item.institutionId
+                                  ..locationId = item.locationId;
+                              })
+                            ],
                           ),
-                          const SizedBox(width: 8),
-                          SvgPicture.asset(
-                            'assets/icons/at.svg',
-                            color: LoonoColors.grey,
-                          ),
-                        ],
+                          builder: (
+                            BuildContext context,
+                            AsyncSnapshot<ApiResponse<HealthcareProviderDetailList>> snapshot,
+                          ) {
+                            if (snapshot.hasData) {
+                              HealthcareProviderDetail? detail;
+                              snapshot.data?.map(
+                                success: (res) {
+                                  detail = res.data.healthcareProvidersDetails.first;
+                                },
+                                failure: (err) {},
+                              );
+                              final hasPhoneNumber =
+                                  detail?.phoneNumber != null && detail!.phoneNumber!.isNotEmpty;
+                              final hasEmail = detail?.email != null && detail!.email!.isNotEmpty;
+
+                              return Row(
+                                children: [
+                                  if (hasPhoneNumber) ...[
+                                    const SizedBox(width: 8),
+                                    SvgPicture.asset(
+                                      'assets/icons/telephone.svg',
+                                      color: LoonoColors.grey,
+                                    ),
+                                  ],
+                                  if (hasEmail) ...[
+                                    const SizedBox(width: 8),
+                                    SvgPicture.asset(
+                                      'assets/icons/at.svg',
+                                      color: LoonoColors.grey,
+                                    ),
+                                  ],
+                                ],
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text(context.l10n.doctor_contact_error);
+                            } else {
+                              return const Flexible(
+                                child: Center(child: CircularProgressIndicator.adaptive()),
+                              );
+                            }
+                          },
+                        ),
                       ],
                     ),
                     Padding(
