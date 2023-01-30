10951f671d405549b664f8ade18c5feaa3506605
magdalenamajakova <magdalena.majakova@gmail.com>
2023-01-30 15:09:41 +0100

    adding newsletter bottom sheet

--- lib/ui/widgets/newsletter/newsletter_bottom_sheet.dart
+++ lib/ui/widgets/newsletter/newsletter_bottom_sheet.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:loono/ui/screens/notification_page.dart';
 
-void showNewsletterBottomSheet(BuildContext context) {
+void showBottomSheet(BuildContext context) {
   showModalBottomSheet<void>(
     context: context,
     isScrollControlled: true,
     builder: (context) => const FractionallySizedBox(
       child: NotificationPage(
-          key: key,
-          notficationType: NotficationType.newsletter,
-        ),
+        notficationType: NotficationType.newsletter,
+      ),
     ),
   );
 }
