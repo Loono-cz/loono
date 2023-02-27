0a8ce759be2e89313605f4593b239fa8faef99f9
Adam Šupej <adam.supej@cgi.com>
2023-02-27 10:34:46 +0100

    fix: moved icon path to LoonoAssets#158

--- lib/ui/widgets/bottom_navigation_builder.dart
+++ lib/ui/widgets/bottom_navigation_builder.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:loono/constants.dart';
 import 'package:loono/l10n/ext.dart';
 import 'package:loono/ui/widgets/custom_navigation_bar.dart';
 import 'package:loono/utils/registry.dart';
@@ -23,18 +24,18 @@ Widget bottomNavigationBuilder(
       CustomNavigationBarItem(
         hasNotification: hasNotification,
         label: context.l10n.main_menu_item_prevention,
-        iconPath: 'assets/icons/tabs/prevention.svg',
-        iconPathActive: 'assets/icons/tabs/prevention_active.svg',
+        iconPath: LoonoAssets.preventionInactiveIcon,
+        iconPathActive: LoonoAssets.preventionIcon,
       ),
       CustomNavigationBarItem(
         label: context.l10n.main_menu_item_find_doc,
-        iconPath: 'assets/icons/tabs/find_doctor.svg',
-        iconPathActive: 'assets/icons/tabs/find_doctor_active.svg',
+        iconPath: LoonoAssets.findDoctorInactiveIcon,
+        iconPathActive: LoonoAssets.findDoctorIcon,
       ),
       CustomNavigationBarItem(
         label: context.l10n.main_menu_item_about_health,
-        iconPath: 'assets/icons/tabs/explore.svg',
-        iconPathActive: 'assets/icons/tabs/explore_active.svg',
+        iconPath: LoonoAssets.exploreInactiveIcon,
+        iconPathActive: LoonoAssets.exploreIcon,
       ),
     ],
   );
