d7efcc44d8c008f4595e2fe953e6f44c51490964
magdalenamajakova <magdalena.majakova@gmail.com>
2023-03-03 15:01:59 +0100

    no message

--- lib/ui/screens/onboarding/fallback_account/submit_account.dart
+++ lib/ui/screens/onboarding/fallback_account/submit_account.dart
@@ -104,5 +104,7 @@ Future<ApiResponse<Account>> _callOnboardUser(
     nickname: user.nickname ?? '',
     preferredEmail: user.email ?? '',
     newsletterOptIn: newsletter,
+    //createdAt: user.createdAt!.toDate(),
+    createdAt: Date.now(),
   );
 }
